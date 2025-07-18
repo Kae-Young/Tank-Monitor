f167acf1100c5fd178d5a741440a5db02b398be7
kae-young <kaeyoung1@outlook.com>
2025-07-19 02:06:39 +1000

    added 2 second sleep at end of buck boost init function

--- src/drivers/buckboost/buckboost.cpp
+++ src/drivers/buckboost/buckboost.cpp
@@ -28,4 +28,6 @@ void buckboost_init()
     buf[0] = 0x04;
     buf[1] = 0b01110010; //was getting vout of 5.14v on the 5v register of the buckboost conveter, changed to the 4.875v register to get an outtput voltage of 5V
     i2c_write_blocking(I2C_INSTANCE, BUCKBOOST_SLAVE_ADDRESS, buf, 2, false);
+
+    sleep_ms(2000);
 }
