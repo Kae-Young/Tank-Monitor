760de47b6247d2ae4e184db98f8c3cfcf3600e21
Kae-Young <kaeyoung1@outlook.com>
2025-08-04 16:27:47 +1000

    update board and C-SR04 header files

--- src/drivers/ultrasonic/HC-SR04.h
+++ src/drivers/ultrasonic/HC-SR04.h
@@ -6,3 +6,7 @@ void ultrasonic_init();
 /// @brief Retrieves distance from ultrasonic sensor
 /// @return 
 int ultrasonic_distance();
+
+/// @brief Calculates actual volume
+/// @return Volume in litres
+float ultrasonic_volume();
